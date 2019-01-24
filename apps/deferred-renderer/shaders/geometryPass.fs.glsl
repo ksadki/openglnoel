43d9359edd7948ea2f1ea0eff3b8e9d496934994
Karim Sadki <ksadki01@etud.u-pem.fr>
2019-01-24 09:05:12 +0100

    deffered in progressing

--- apps/deferred-renderer/shaders/geometryPass.fs.glsl
+++ apps/deferred-renderer/shaders/geometryPass.fs.glsl
@@ -4,7 +4,12 @@ in vec3 vFragPositionVS;
 in vec3 vFragNormalVS;
 in vec2 vFragTex;
 
-out vec3 fFragColor;
+//out vec3 fFragColor;
+layout(location = 0) out vec3 fPosition;
+layout(location = 1) out vec3 fNormal;
+layout(location = 2) out vec3 fAmbient;
+layout(location = 3) out vec3 fDiffuse;
+layout(location = 4) out vec4 fGlossyShininess;
 
 uniform vec3 uDirectionalLightDir;
 uniform vec3 uDirectionalLightIntensity;
@@ -21,27 +26,27 @@ uniform vec3 uKd;
 uniform vec3 uKs;
 uniform float uShininess;
 
-vec3 blinnPhong(vec3 normal, vec3 intensity, vec3 outDir, vec3 incDir){
+/*vec3 blinnPhong(vec3 normal, vec3 intensity, vec3 outDir, vec3 incDir){
 	vec3 Kd = texture(uKdSampler, vFragTex).rgb * uKd;
 	vec3 Ks = texture(uKsSampler, vFragTex).rgb * uKs;
 
     vec3 halfVector = (outDir + incDir)/2;
     vec3 vec = intensity*((Kd*dot(incDir, normal)) + (Ks * pow(dot(halfVector, normal), uShininess)));
     return vec;
-}
+}*/
 
 void main(){
 	
-	float distToPointLight = length(uPointLightPosition -vFragPositionVS);
+	/*float distToPointLight = length(uPointLightPosition -vFragPositionVS);
 	vec3 dirToPointLight = (uPointLightPosition - vFragPositionVS) / distToPointLight;
 	vec3 Ka = texture(uKaSampler, vFragTex).rgb * uKa;
 
 	fFragColor = blinnPhong(normalize(vFragNormalVS), uDirectionalLightIntensity, normalize(-vFragPositionVS), uDirectionalLightDir) +
-				 blinnPhong(normalize(vFragNormalVS), uPointLightIntensity / (distToPointLight * distToPointLight), normalize(-vFragPositionVS), dirToPointLight)+Ka;
+				 blinnPhong(normalize(vFragNormalVS), uPointLightIntensity / (distToPointLight * distToPointLight), normalize(-vFragPositionVS), dirToPointLight)+Ka;*/
 
-	/*fPosition = ;
-	fNormal = ;
-	fAmbient = ;
-	fDiffuse = ;
-	fGlossyShininess = ;*/
+	fPosition = vFragPositionVS;
+	fNormal = vFragNormalVS;
+	fAmbient = texture(uKaSampler, vFragTex).rgb * uKa;
+	fDiffuse = texture(uKdSampler, vFragTex).rgb * uKd;
+	fGlossyShininess = vec4(texture(uKsSampler, vFragTex).rgb * uKs,uShininess);
 }
